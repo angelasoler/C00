9f83bba252f577101a24f7a0d9f1e0992c7f08d9
asoler <asoler@student.42sp.org.br>
2022-02-03 22:52:09 +0100

    created put funtion to do de write job into ft_print_comb

--- ex05/ft_print_comb.c
+++ ex05/ft_print_comb.c
@@ -6,25 +6,37 @@
 /*   By: asoler <asoler@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 01:22:55 by asoler            #+#    #+#             */
-/*   Updated: 2022/02/03 20:03:33 by asoler           ###   ########.fr       */
+/*   Updated: 2022/02/03 22:47:33 by asoler           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-// void	put(char c)
-// {
-// 	write(1, &c, 1)
-// }
 
-void	ft_print_comb(void)
+void	put(char*c)
 {
-	char	digit[3];
 	char	separator[3];
 
-	digit[0] = '0';
 	separator[0] = ',';
 	separator[1] = ' ';
 	separator[2] = '\n';
+	if (c[0] < '7')
+	{
+		write(1, c, 3);
+		write(1, separator, 2);
+	}
+	if (c[0] == '7')
+	{
+		write(1, c, 3);
+		write(1, &separator[2], 1);
+	}
+}
+
+void	ft_print_comb(void)
+{
+	char	digit[4];
+
+	digit[0] = '0';
+	digit[3] = '\n';
 	while (digit[0] <= '7')
 	{
 		digit[1] = digit[0] + 1;
@@ -33,15 +45,7 @@ void	ft_print_comb(void)
 			digit[2] = digit[1] + 1;
 			while (digit[2] <= '9')
 			{
-				write(1, digit, 3);
-				if (digit[0] != '7')
-				{
-					write(1, separator, 2);
-				}
-				else
-				{
-					write(1, &separator[2], 1);
-				}
+				put(digit);
 				digit[2]++;
 			}
 			digit[1]++;
